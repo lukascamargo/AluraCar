f3be9d89cbb904e55fb9134e5b46753182ddd63f
Lukas Camargo <lukas.fialho@gmail.com>
2017-02-18 01:39:50 -0200

    first commit

---
+++ www/js/controller.js
@@ -0,0 +1,12 @@
+angular.module('starter')
+	.controller('ListagemController', function($scope){
+		$scope.listaDeCarros = ['BMW 120i','Onix 1.6', 'Fiesta 2.0', 'C3 1.0', 
+								'Uno Fire','Sentra 2.0', 'Astra Sedan', 'Vectra 2.0 Turbo', 
+								'Hilux 4x4', 'Montana Cabine Dupla', 'Outlander 2.4', 
+								'Fusca 1500' ];
+		$scope.footerMessage = "Rua Vergueiro, 3185";
+
+	})
+	.controller('CarroEscolhidoController', function($scope){
+
+	});
